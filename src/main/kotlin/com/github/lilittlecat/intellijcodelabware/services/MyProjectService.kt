46c19f863fe59ed7e34915b0078f3db4a9388497
GitHub Action <action@github.com>
2023-02-21 03:40:13 +0000

    Template cleanup

--- src/main/kotlin/com/github/lilittlecat/intellijcodelabware/services/MyProjectService.kt
+++ src/main/kotlin/com/github/lilittlecat/intellijcodelabware/services/MyProjectService.kt
@@ -1,7 +1,7 @@
-package org.jetbrains.plugins.template.services
+package com.github.lilittlecat.intellijcodelabware.services
 
 import com.intellij.openapi.project.Project
-import org.jetbrains.plugins.template.MyBundle
+import com.github.lilittlecat.intellijcodelabware.MyBundle
 
 class MyProjectService(project: Project) {
 
