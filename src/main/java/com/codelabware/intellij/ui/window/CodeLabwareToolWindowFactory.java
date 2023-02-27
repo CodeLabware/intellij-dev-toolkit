3ecf90e92676e2a50392a09c2ef27b01cdfedd42
LiLittleCat <luoyukongchan@outlook.com>
2023-02-27 19:08:26 +0800

    Move files

--- src/main/java/com/codelabware/intellij/ui/window/CodeLabwareToolWindowFactory.java
+++ src/main/java/com/codelabware/intellij/ui/window/CodeLabwareToolWindowFactory.java
@@ -1,19 +1,14 @@
-package com.codelabware.intellij.window;
+package com.codelabware.intellij.ui.window;
 
 import com.codelabware.intellij.message.CodeLabwareBundle;
 import com.codelabware.intellij.view.editor.Editor;
-import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.project.Project;
 import com.intellij.openapi.wm.ToolWindow;
 import com.intellij.openapi.wm.ToolWindowFactory;
-import com.intellij.ui.EditorComboBox;
 import com.intellij.ui.EditorTextField;
 import com.intellij.ui.content.Content;
 import com.intellij.ui.content.ContentManager;
 import org.jetbrains.annotations.NotNull;
-
-import java.util.ArrayList;
-import java.util.List;
 
 /**
  * @author <a href="https://github.com/LiLittleCat">LiLittleCat</a>
@@ -24,12 +19,9 @@ public class CodeLabwareToolWindowFactory implements ToolWindowFactory {
     public void createToolWindowContent(@NotNull Project project, @NotNull ToolWindow toolWindow) {
         ContentManager contentManager = toolWindow.getContentManager();
 
-
 //        Content labelContent = contentManager.getFactory().createContent(new Editor(project, Editor.JSON_FILE_TYPE), CodeLabwareBundle.message("plugin.name"), false);
         Content labelContent = contentManager.getFactory().createContent(new EditorTextField(project, Editor.JSON_FILE_TYPE), CodeLabwareBundle.message("plugin.name"), false);
-        Content labelContent1 = contentManager.getFactory().createContent(new EditorComboBox("test"), CodeLabwareBundle.message("plugin.name"), false);
         contentManager.addContent(labelContent);
-        contentManager.addContent(labelContent1);
 //        // add actions to tool window
 //        List<AnAction> anActionList = new ArrayList<>();
 //        anActionList.add(new SettingsAction("Settings"));
